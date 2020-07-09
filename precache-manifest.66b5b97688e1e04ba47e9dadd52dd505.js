a80d2a10aa7ebe36435a95d530fc921861939ae7
Chris Lander <chrislander@gmail.com>
2020-07-09 18:29:36 +0100

    Updates

---
+++ precache-manifest.66b5b97688e1e04ba47e9dadd52dd505.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc92fe6cc00aff94a159af6357d284cf",
+    "url": "/algo-app/index.html"
+  },
+  {
+    "revision": "b73b4fef786add28f36b",
+    "url": "/algo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8aa92ac33b462ca5fb7",
+    "url": "/algo-app/static/js/2.9e5b20d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/algo-app/static/js/2.9e5b20d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73b4fef786add28f36b",
+    "url": "/algo-app/static/js/main.73f7f22d.chunk.js"
+  },
+  {
+    "revision": "2c681de835735d96e77f",
+    "url": "/algo-app/static/js/runtime-main.83272c52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/algo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
